89cc5f50ee20d3a515489c3bbadb5391f6fd3f4a
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-08-20 14:30:46 +0200

    landing ui

--- src/app/landingpage/video.tsx
+++ src/app/landingpage/video.tsx
@@ -269,7 +269,7 @@ export const VideoSection = () => {
           <h2 className="text-3xl sm:text-4xl lg:text-6xl font-robotoCondensed font-bold text-black">
             Your MINI adventure starts now.
           </h2>
-          <p className="pt-4 text-lg md:text-xl text-stone-600/90 font-roboto">
+          <p className="pt-4 text-lg md:text-xl text-stone-600 font-roboto">
             Desktop — hover to play. Mobile — auto-play on view.
           </p>
         </div>
