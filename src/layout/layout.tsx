a8a54b3fe704a875273b9317f8757064bbc4f919
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-08-29 14:33:46 +0200

    ui main layout

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -66,49 +66,58 @@ export default function Layout() {
 
   const SidebarMenu = () => {
     return (
-      <ul className="flex flex-col gap-1 text-left">
-        {menuItems.map(({ to, icon, label, exact, onClick }) => (
-          <li key={to} className="rounded-md hover:bg-zinc-100 cursor-pointer">
-            <NavLink
-              to={to}
-              end={exact} // ✅ end отключает частичное совпадение
-              className={({ isActive }) =>
-                `flex w-full h-full items-center justify-between p-3 ${
-                  isActive ? "bg-zinc-100 rounded-md" : ""
-                }`
-              }
-              onClick={onClick}
+      <>
+        <NavLink
+          to={"/"}
+          // className=" ml-1 flex px-3 py-2 items-center font-openSans font-bold text-xl"
+          className="flex flex-col items-center shrink-0 font-roboto uppercase font-bold"
+        >
+          <img src="/icons/logow.png" className=" w-16" />
+          <p className=" text-white text-xs">MINI2GO</p>
+        </NavLink>
+        <ul className="flex flex-col gap-1 text-left mt-5">
+          {menuItems.map(({ to, icon, label, exact, onClick }) => (
+            <li
+              key={to}
+              className="rounded-md hover:bg-gradient-to-r hover:from-teal-900/40 from-30% hover:to-indigo-900/5 cursor-pointer text-white"
             >
-              <div className="flex items-center gap-2">
-                {icon}
-                {label}
-              </div>
-              {/* {count > 0 && <Badge variant="default">{count}</Badge>} */}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
+              <NavLink
+                to={to}
+                end={exact} // ✅ end отключает частичное совпадение
+                className={({ isActive }) =>
+                  `flex w-full h-full items-center justify-between p-3 ${
+                    isActive
+                      ? "bg-gradient-to-r from-teal-900/90 from-30%  to-indigo-900/10 rounded-md"
+                      : ""
+                  }`
+                }
+                onClick={onClick}
+              >
+                <div className="flex items-center gap-2">
+                  {icon}
+                  {label}
+                </div>
+                {/* {count > 0 && <Badge variant="default">{count}</Badge>} */}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </>
     );
   };
 
   return (
     <AppShell
+      layout="alt"
       header={{ height: 60 }}
       navbar={{
         width: 300,
         breakpoint: "md",
         collapsed: { mobile: !opened },
       }}
-      // padding="md"
       padding={{ base: "md", sm: "lg", md: "xl", lg: "xl" }}
     >
-      <AppShell.Header className=" flex items-center justify-between">
-        <NavLink
-          to={"/"}
-          className=" ml-1 flex px-3 py-2 items-center font-openSans font-black text-2xl"
-        >
-          MINI2go
-        </NavLink>
+      <AppShell.Header className=" flex items-center justify-end">
         <Burger
           opened={opened}
           onClick={toggle}
@@ -116,15 +125,30 @@ export default function Layout() {
           size="sm"
           mx={10}
         />
+
         <div className=" hidden lg:block px-4">
           <UserMenu onClick={toggle} />
         </div>
       </AppShell.Header>
 
-      <AppShell.Navbar p="md">
-        <div>{SidebarMenu()}</div>
-        <div className="lg:hidden fixed bottom-2">
-          <UserMenu onClick={toggle} />
+      <AppShell.Navbar p="xs" bg={"white"}>
+        {/* #102d20cc #073b25  #184230*/}
+        <div className=" absolute right-2 top-5">
+          <Burger
+            opened={opened}
+            onClick={toggle}
+            hiddenFrom="md"
+            size="sm"
+            mx={10}
+            color="white"
+          />
+        </div>
+
+        <div className="h-full bg-gradient-to-r from-teal-950 from-5% to-emerald-800 to-95% p-3">
+          <div>{SidebarMenu()}</div>
+          <div className="lg:hidden fixed bottom-2">
+            <UserMenu onClick={toggle} />
+          </div>
         </div>
       </AppShell.Navbar>
 
