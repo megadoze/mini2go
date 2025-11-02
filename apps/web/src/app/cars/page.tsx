cdbdadbdf9c7519a08697a938e7a53c2af90a1b5
Oleg <Oleg@macs-MacBook-Pro-4.local>
2025-11-02 15:37:50 +0100

    cars page meta

--- apps/web/src/app/cars/page.tsx
+++ apps/web/src/app/cars/page.tsx
@@ -1,6 +1,14 @@
-"use client";
-
+import { Metadata } from "next";
 import CatalogClient from "./catalogClient"; // или как ты назвал файл
+
+export const metadata: Metadata = {
+  title: "Cars - MINI2GO",
+  description: "Fleet MINI2GO. Browse and rent cars.",
+  openGraph: {
+    title: "Cars - MINI2GO",
+    description: "Fleet MINI2GO",
+  },
+};
 
 export default function CatalogPage() {
   return <CatalogClient />;
